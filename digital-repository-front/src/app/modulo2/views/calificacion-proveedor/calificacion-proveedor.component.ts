9cbe562c4b6edb3a4db858b8f63cc08aa6cf7341
cjaneth <Cjaneth@unicauca.edu.co>
2023-10-29 15:33:09 -0500

    se cambio el input y el boton, y dio un index para cada vista

--- digital-repository-front/src/app/modulo2/views/calificacion-proveedor/calificacion-proveedor.component.ts
+++ digital-repository-front/src/app/modulo2/views/calificacion-proveedor/calificacion-proveedor.component.ts
@@ -15,4 +15,10 @@ export class CalificacionProveedorComponent {
     {criterio: 'criterio 2 ', descripcion: 'Nulla facilisi. Aenean vel justo at risus tincidunt lobortis. Ut non tincidunt ante, vitae aliquet enim. Maecenas non semper ante. Sed vitae augue nec massa tincidunt posuere. Vivamus fringilla, turpis at volutpat accumsan, ipsum nisi luctus tellus, sit amet commodo nulla metus ut neque.'},
     {criterio: 'criterio 3 ', descripcion: 'Quisque non neque et sapien scelerisque finibus ut sit amet nunc. Nunc eu erat tristique, finibus neque in, tincidunt odio. Nullam sagittis sem in leo vulputate, sit amet tincidunt massa rhoncus. Integer nec hendrerit purus, sed tempor justo. In convallis risus vitae aliquet molestie. Sed malesuada dolor in nibh aliquet, a bibendum dui auctor.'},
   ];
+  
+   indexVistaActual: number = 0; // por defecto la primera vista se ve 0
+   indexVistaMax: number = (this.tiles.length-1) +2; //total de calificaciones(-1 POR SER INDICE)+ 2 vistas mas (busqueda y resultados)
+  constructor(){
+
+  }
 }
