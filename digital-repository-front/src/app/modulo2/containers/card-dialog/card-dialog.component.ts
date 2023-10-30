32beb6cfb3dbfe54c170fba26151e1bfaa4628df
cjaneth <Cjaneth@unicauca.edu.co>
2023-10-30 10:55:25 -0500

    navegabilidad entre radios y opciones pasadas
    se puede navegar con el boton siguiente y atras y las opciones aun no confirmadas perduran

--- digital-repository-front/src/app/modulo2/containers/card-dialog/card-dialog.component.ts
+++ digital-repository-front/src/app/modulo2/containers/card-dialog/card-dialog.component.ts
@@ -1,19 +1,20 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-card-dialog',
   templateUrl: './card-dialog.component.html',
   styleUrls: ['./card-dialog.component.css']
 })
-export class CardDialogComponent {
+export class CardDialogComponent implements OnInit {
   @Input() criterio: string="";
   @Input() descripcionCriterio: string="";
   @Input() contBotones:number=0;
   @Output() emisorNavegacion = new EventEmitter<number>();
-
+  @Output() emisorValor = new EventEmitter<number>();
+  @Input() seleccionadoRadio!:number;
+  seleccionado=0;
   checked = false;
   indeterminate = false;
-  labelPosition: 'before' | 'after' = 'after';
   disabled = false;
   dicPuntaje: { [key: number]: string } = {
     1: "No cumple",
@@ -22,7 +23,11 @@ export class CardDialogComponent {
     4: "Plenamente",
     5: "Supera expectativas"
   };
-  
+  constructor(){
+  }
+  ngOnInit(): void {
+    alert(this.seleccionadoRadio +" <- FORMULRIO RECIBE EL ");
+  }
   getPuntaje() {
     return Object.keys(this.dicPuntaje);
   }
@@ -32,5 +37,15 @@ export class CardDialogComponent {
   navegar(varNavegacion:number)
   {
     this.emisorNavegacion.emit(varNavegacion);
+    this.almacenaValor( this.seleccionadoRadio);//cuando no sea cambiado el valor disparamos el metodo automaticamente para no perder cambios
+    this.emisorValor.emit(this.seleccionadoRadio);
+  }
+  /***
+   * cuando se cambia el radio se dispara este metodo para almacenar el valor
+   */
+  almacenaValor(value: any) {
+    this.seleccionadoRadio = value; // Actualiza el valor seleccionado
+    console.log('El valor seleccionado es: ' + this.seleccionadoRadio);
+
   }
 }
--- digital-repository-front/src/app/modulo2/views/calificacion-proveedor/calificacion-proveedor.component.ts
+++ digital-repository-front/src/app/modulo2/views/calificacion-proveedor/calificacion-proveedor.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 export interface Tile {
   criterio : string;
   descripcion : string;
@@ -9,17 +9,24 @@ export interface Tile {
   templateUrl: './calificacion-proveedor.component.html',
   styleUrls: ['./calificacion-proveedor.component.css']
 })
-export class CalificacionProveedorComponent {
+export class CalificacionProveedorComponent implements OnInit {
   tiles: Tile[] = [
     {criterio: 'criterio 1 ', descripcion: 'Lorem ipsum dolo'},
     {criterio: 'criterio 2 ', descripcion: 'Nulla facilisi. Aenean vel justo at risus tincidunt lobortis. Ut non tincidunt ante, vitae aliquet enim. Maecenas non semper ante. Sed vitae augue nec massa tincidunt posuere. Vivamus fringilla, turpis at volutpat accumsan, ipsum nisi luctus tellus, sit amet commodo nulla metus ut neque.'},
     {criterio: 'criterio 3 ', descripcion: 'Quisque non neque et sapien scelerisque finibus ut sit amet nunc. Nunc eu erat tristique, finibus neque in, tincidunt odio. Nullam sagittis sem in leo vulputate, sit amet tincidunt massa rhoncus. Integer nec hendrerit purus, sed tempor justo. In convallis risus vitae aliquet molestie. Sed malesuada dolor in nibh aliquet, a bibendum dui auctor.'},
   ];
-  
+   calificacionesArray: number[] = [];
    indexVistaActual: number = 0; // por defecto la primera vista se ve 0
    indexVistaMax: number = (this.tiles.length-1) +2; //total de calificaciones(-1 POR SER INDICE)+ 2 vistas mas (busqueda y resultados)
   
    numContrato:string="";
+  constructor(){
+    this.calificacionesArray = new Array(this.tiles.length).fill(0);
+  }
+   
+  ngOnInit() {
+    window.onbeforeunload = () => this.confirmExit();
+  }
   /**
    * navega a la proxima pagina y hace un llamado a recupera datos necesarios
    * @param contrato es el identificador del contrato ya balidado por la primera interfaz
@@ -40,14 +47,18 @@ export class CalificacionProveedorComponent {
   comprobarvista(){
 
   }
-
+  /**
+   * cuando un criterio es calificado lo llenamos en el arreglo
+   */
+  ingresarCalificacionCriterio(calificado:number, indice:number){
+    console.log("caificacionResivida"+calificado+" para criterio '"+this.tiles[indice].criterio+"'");
+    this.calificacionesArray[indice]=calificado
+    alert(this.calificacionesArray);
+  }
   @HostListener('window:beforeunload', ['$event'])
   beforeUnloadHandler(event: Event) {
     event.returnValue = true; 
     return '¿Está seguro de abandonar la página? Los datos no guardados se perderán.';
-  }
-  ngOnInit() {
-    window.onbeforeunload = () => this.confirmExit();
   }
 
   confirmExit() {
